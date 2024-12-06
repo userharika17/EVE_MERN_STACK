82bd502a83ed700a1621255de032ab22536cc1c9
Harika <21jr1a12e5@gmail.com>
2024-12-06 16:49:02 +0530

    first commit

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      At EVE, we believe that every celebration deserves to be extraordinary. From intimate gatherings to grand ceremonies, we specialize in transforming your ideas into unforgettable experiences.Our platform offers end-to-end event planning solutions, tailored to your unique needs. Whether it’s a wedding, birthday, corporate event, or social gathering, we ensure seamless execution, allowing you to relax and enjoy every moment.With a perfect blend of creativity, innovation, and precision, we focus on delivering not just events but memories that last a lifetime. Our passionate team works tirelessly to bring your vision to life, making every detail count.Let EVE be your trusted partner in creating magical moments. Together, we’ll celebrate life’s most cherished milestones with style, elegance, and perfection.
+      <b>Let’s Plan, Celebrate, and Cherish Together!</b>
+      </p>
+    </section>
+  );
+};
+
+export default About;
